63064f8446314483d61d444c69eb0ff3d3551c60
javidrashkhansoi <cavidgulmaliyev491995@gmail.com>
2022-01-01 01:34:42 +0400

    add class 'popular' and add slider to this class

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 	$(".exclusive__slider").slick({
-		arrows: true,
 		adaptiveHeight: true,
 		speed: 1000,
 		autoplay: true,
@@ -13,5 +12,23 @@ $(document).ready(function () {
 		touchMove: true,
 		waitForAnimate: true,
 		appendArrows: $(".exclusive__buttons>div"),
-	})
+	});
+	$(".popular__slider").slick({
+		arrows: true,
+		slidesToShow: 3,
+		// speed: 1000,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		// infinite: true,
+		draggable: true,
+		swipe: true,
+		touchMove: true,
+		waitForAnimate: true,
+		// centerMode: true,
+		// variableWidth: true,
+		appendArrows: $(".popular__buttons"),
+	});
 })
